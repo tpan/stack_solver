a6c46e64979cfbb5bd3c49a386f3df2eb834b3cc
Thomas Pan <tpan@e1z2r5p11.42.us.org>
2017-05-13 12:28:27 -0700

    woops should have pushed before i left for palo alto

--- srcs/swap_ops.c
+++ srcs/swap_ops.c
@@ -6,7 +6,7 @@
 /*   By: tpan <tpan@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 20:59:49 by tpan              #+#    #+#             */
-/*   Updated: 2017/05/05 21:18:08 by tpan             ###   ########.fr       */
+/*   Updated: 2017/05/11 18:17:03 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,18 @@ void		sa(t_swap **stacka, t_swap **stackb, t_tracker *tracker)
 
 void		sb(t_swap **stacka, t_swap **stackb, t_tracker *tracker)
 {
-	if ((stack_length(*stacka) < 2) || stack_length(*stackb) < 2))
+	if ((stack_length(*stacka) < 2) || (stack_length(*stackb) < 2))
 		return ;
 
-	
+	flip_the_top(stackb);
+	tracker->counter++;
+	print_output(stacka, stackb, tracker);
+}
+
+void		ss(t_swap **stacka,t_swap **stackb, t_tracker *tracker)
+{
+	flip_the_top(stacka);
+	flip_the_top(stackb);
+	tracker->counter++;
+	print_output(stacka, stackb, tracker);
 }
